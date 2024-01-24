fffb0e9175fe081ee534a37207531c36ab7454a6
BartZap <147654294+BartZap@users.noreply.github.com>
2024-01-24 16:17:10 +0100

    Delete account.cpp

--- account.cpp
+++
@@ -1,7 +0,0 @@
-#include "account.h"
-void Account::logged_in(){
-    std::cout << "---Welcome back---" << std::endl;
-    std::cout << "1. Generate new password." << std::endl;
-    std::cout << "2. Show my data." << std::endl;
-    
-}
