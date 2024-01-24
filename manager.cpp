37dd08a3e539946daf7e6b791b7b687cbb298d76
BartZap <147654294+BartZap@users.noreply.github.com>
2024-01-24 16:17:21 +0100

    Add files via upload

--- manager.cpp
+++ manager.cpp
@@ -1,5 +1,10 @@
 #include "manager.h"
-#include "account.h"
+
+#ifdef _WIN32
+#define CLEAR "cls"
+#else 
+#define CLEAR "clear"
+#endif
 
 Manager::Manager() = default;
 Manager::~Manager() = default;
@@ -18,26 +23,43 @@ void Manager::start_screen(){
         switch(choice){
             case 1:
             {
+                //Dziala
                 log_in();
                 break;
             }
             case 2:
-            {
-                //PROBLEM PRZY PODAWANIU TEGO SAMEGO LOGINU
-                //USUWA ZAWARTOSC PLIKU TXT
-                over_write();
+            {   
+                //dziala
+                system(CLEAR);
+                read_users();
+                std::cout << "Podaj login : " ;
+                std::cin >> login ;
+                std::cout << "Podaj haslo : ";
+                std::cin >> password;
+                auto can_can = logs.find(login);
+                try{
+                    if(can_can == logs.end()){//czy nie istnieje juz taki login
+                        logs[login] = password;
+                        std::cout << "Utworzono konto." << std::endl;
+                        over_write();
+                    }else
+                        throw "Podany login istnieje" ;
+                }catch(const char* exception){
+                    std::cout << exception << std::endl;
+                }
                 break;
             }
             case 3:
             {
-                system("cls");
+                //Dziala
+                system(CLEAR);
                 std::cout << "Bye" << std::endl;
-                steer = false;
+                return;
                 break;
             }
             default:
             {
-                system("cls");
+                system(CLEAR);
                 std::cout << "Wrong action !" << std::endl;
                 break;
             }
@@ -45,7 +67,7 @@ void Manager::start_screen(){
     }
 }
 void Manager::create_account(){
-    system("cls");
+    system(CLEAR);
     std::cout << "Enter your login : ";
     std::cin >> login;
     std::cout << "Enter your password : ";
@@ -59,19 +81,19 @@ void Manager::create_account(){
         std::cout << "Passed login already exists in database, please change it." << std::endl;
     }
 }
-void Manager::read_users(){
+void Manager::read_users(std::string sciezka){
     //wpisanie do mapy zeby dola rade szukac istniejace loginy
-    plik.open("napisy.txt",std::ios::out);
+    plik.open(sciezka,std::ios::out);
     int nr_linii = 1;
     while(std::getline(plik,napis)){
         //zczytywanie pliku
         switch(nr_linii){
             case 1:
-                login = napis;
+                domain = napis;
                 break;
             case 0:
                 password = napis;
-                logs[login] = password;
+                logs[domain] = password;
                 break;
             default:
                 std::cout << "Bad file" << std::endl;
@@ -82,25 +104,10 @@ void Manager::read_users(){
     }
     plik.close();
 }
-void Manager::over_write(){
+void Manager::over_write(std::string sciezka){
     //nadpisywanie uzytkownikow 
-    system("cls");
-    read_users();
-    plik2.open("napisy.txt",std::ios::trunc);
-    std::cout << "Podaj login : " ;
-    std::cin >> login ;
-    std::cout << "Podaj haslo : ";
-    std::cin >> password;
-    auto can_can = logs.find(login);
-    try{
-        if(can_can == logs.end())//czy nie istnieje juz taki login
-            logs[login] = password;
-        else
-            throw "Podany login istnieje" ;
-    }catch(const char* exception){
-        std::cout << exception << std::endl;
-    }
-    can_can = logs.begin();
+    plik2.open(sciezka,std::ios::trunc);
+    auto can_can = logs.begin();
     while(can_can != logs.end()){
         //wpisywanie do pliku
         plik2 << can_can -> first;
@@ -112,7 +119,7 @@ void Manager::over_write(){
     plik2.close();
 }
 void Manager::log_in(){
-    system("cls");
+    system(CLEAR);
     std::cout << "Podaj swoj login : ";
     std::cin >> login;
     std::cout << "Podaj haslo : ";
@@ -120,9 +127,67 @@ void Manager::log_in(){
     auto check = logs.find(login);
     if(check != logs.end()){
         if(check -> second == password)
-            std::cout << "Udane logowanie !" << std::endl;
+            private_account();
     }else{
         std::cout << "Nie poprawny login lub haslo." << std::endl;
         start_screen();
     }
+}
+std::string Manager::get_login(){
+    return login;
+}
+void Manager::private_account(){
+    system(CLEAR);
+    int choose{};
+    std::cout << "Udane logowanie !" << std::endl;
+    while(1){
+        std::cout << "---Welcome back---" << std::endl;
+        std::cout << "1. Generate new password." << std::endl;
+        std::cout << "2. Show my logs." << std::endl;
+        std::cout << "3. Log out." << std::endl;
+        std::cin >> choose;
+        switch(choose){
+            case 1:
+                generate_pass();
+                break;
+            case 2:
+                show_my_info();
+                break;
+            case 3:
+                return;
+                break;
+            default:
+                std::cout << "Wrong action." << std::endl;
+                break;
+        }
+    }
+}
+void Manager::show_my_info(){
+    //zczytuje osobisty plik z haslami i witrynami
+    logs.clear();
+    read_users("hasla\\"+login+".txt");
+    system(CLEAR);
+    std::cout << "---------------" << std::endl;
+    for(auto a : logs){
+        std::cout << "[ Witryna : " << a.first << ", Haslo : " << a.second << " ]" << std::endl;
+        std::cout << "---------------" << std::endl;
+    }
+    system("pause");
+    system(CLEAR);
+}
+void Manager::generate_pass(){
+    std::string domena{};
+    std::random_device rd;
+    std::uniform_int_distribution<int> distribution{33,127};//zgodnie z tablica ascii
+    std::cout << "Podaj witryne do ktorej chcesz utworzyc nowe haslo." ;
+    std::cin >> domena;
+    distribution(rd);
+    //wyczyscic mape i wpisac do niej dane poprzednie z pliku
+    logs.clear();
+    read_users("hasla\\"+login+".txt");
+    for(int i = 0 ; i < MAX_PASS_LEN; i++){
+        pass[i] = static_cast<char>(distribution(rd));
+    }
+    logs[domena] = pass;
+    over_write("hasla\\"+login+".txt");
 }
--- manager.h
+++ manager.h
@@ -6,22 +6,30 @@
 #include <algorithm>
 #include <map>
 #include <fstream>
+#include <vector>
+#include <random>
 
 class Manager{
 private:
-    std::string napis{},login{},password{};
+    static const int MAX_PASS_LEN = 20;
+    std::string napis{},domain{},password{},login{};
     size_t y{};
     std::ifstream plik;
     std::ofstream plik2;
     std::map<std::string,std::string> logs{};
+    char pass[MAX_PASS_LEN];
 public:
     Manager();
     ~Manager();
+    std::string get_login();
     void start_screen();
     void create_account();
     void check_login();
-    void read_users();
-    void over_write();
+    void read_users(std::string sciezka = "napisy.txt");
+    void over_write(std::string sciezka = "napisy.txt");
     void log_in();
+    void private_account();
+    void generate_pass();
+    void show_my_info();
 };
 #endif
